e2f9dfa9847763d16784f7698664b6f7e6e412e4
Jeel darji <darjijeel31@gmail.com>
2026-01-02 15:27:33 +0530

    come

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client"
 import "server-only"
 
-const projectId = process.env.SANITY_PROJECT_ID||'demo'
+const projectId = process.env.SANITY_PROJECT_ID
 const dataset = process.env.SANITY_DATASET || "production"
 const apiVersion = process.env.SANITY_API_VERSION || "2024-01-01"
 const useCdn = process.env.NODE_ENV === "production"
