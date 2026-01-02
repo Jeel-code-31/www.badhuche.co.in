dc275ea198ad63d9fa66708b838e7477eac7391a
Jeel darji <darjijeel31@gmail.com>
2026-01-02 15:14:37 +0530

    worked

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,9 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-
+import {structureTool} from 'sanity/structure'
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
-
+import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
@@ -12,12 +12,9 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-  
+    structureTool({structure}),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
-  document: {
-    // Prevent accidental unpublishing of documents
-    actions: (input) =>
-      input.filter(({ action }) => action !== 'unpublish'),
-  },
 })
